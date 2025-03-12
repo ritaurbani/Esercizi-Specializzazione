6a8bc6683a7e5725dfd984b4f7b4dbb12a22191d
ritaurbani <ritaurbani@gmail.com>
2025-03-12 11:00:20 +0100

    feat: added more solutions

--- getNegativeNum.js
+++ getNegativeNum.js
@@ -1,13 +1,27 @@
 // 3) Write a function that takes a number and return the negative version of that number.
 
-//DOMANDA = altre possibili soluzioni??
 
-const getNegativeNum = (num) => {
-  if(num > 0) {
-    return num - (num*2)
-} else {
-    return num
-}
-}
+// const getNegativeNum = (num) => {
+//   if(num > 0) {
+//     return num - (num*2)
+// } else {
+//     return num
+// }
+// }
+// console.log(getNegativeNum(8))
+///////////////////////
+// const getNegativeNum = (num) => num * -1
+// console.log(getNegativeNum(8))
+///////////////////////
+// const getNegativeNum = (num) => {
+//   return num > 0 ? -num : num;
+// }
 
-console.log(getNegativeNum(-7))
+// console.log(getNegativeNum(7))
+//////////////////////
+// const getNegativeNum = (num) => {
+//   return -Math.abs(num)
+// }
+
+// console.log(getNegativeNum(8))
+///////////////////////
