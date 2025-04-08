f2c13714ced527baae24ada1124f8a2606b7a9c0
ritaurbani <ritaurbani@gmail.com>
2025-04-08 16:29:53 +0200

    2.isAllLetters

---
+++ 2.isAllLetters.js
@@ -0,0 +1,16 @@
+
+// (MEDIUM) 6. *isAllLetters*: Check if a string contains only letters.
+//    isAllLetters("abc123") → false 
+
+const isAllLetters = (str) => {
+   const nums = [1,2,3,4,5,6,7,8,9,0]
+    for(let num of nums){
+        if (str.includes(num)){
+            return false
+        }
+    }
+    return false
+    };
+
+
+   isAllLetters("abc123")
