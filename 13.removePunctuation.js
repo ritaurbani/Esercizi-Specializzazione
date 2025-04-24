00a014bdc30d49c4a4bda29c61127e43404f3ff9
ritaurbani <ritaurbani@gmail.com>
2025-04-24 10:31:24 +0200

    v

--- 13.removePunctuation.js
+++ 13.removePunctuation.js
@@ -1,6 +1,15 @@
 // (MEDIUM) 9. * removePunctuation *: Remove punctuation from a string.
 //         removePunctuation("Hello!") → "Hello"
 
-const removePunctuation = () => {
-    
-}
+const removePunctuation = (string) => {
+    const punctuation = ".,?!:;"
+    const newPunctuation = punctuation.split("")
+    let newArr;
+    newPunctuation.forEach((p)=> {
+    newArr = string.split("").filter((elem) => elem !== p)
+    console.log("controllo",newArr)
+
+})
+return newArr}
+
+console.log(removePunctuation("Hello!"))
--- 14.toCamelCase.js
+++ 14.toCamelCase.js
@@ -1,2 +1,9 @@
 // (MEDIUM)  10. * toCamelCase *: Convert hyphenated string to camelCase.
 //                 toCamelCase("hello-world") → "helloWorld"
+
+
+const toCamelCase = () => {
+    
+}
+
+console.log(toCamelCase("hello-world"))
